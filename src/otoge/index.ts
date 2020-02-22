e2a7c321ed09891c630bbcdcc0e8c966a8d64868
takagiy <takagiy.4dev@gmail.com>
2020-02-22 20:25:41 +0900

    add title printing

--- src/otoge/index.ts
+++ src/otoge/index.ts
@@ -4,6 +4,15 @@ import * as Path from "path";
 import { OtogeSendable } from "./commands/send";
 import { otogeReceivable } from "./commands/receive";
 
+const title = String.raw`
+ __  __  ____  ____  ____  _____  _  _   _____
+(  \/  )( ___)(_  _)(  _ \(  _  )( \( ) (  _  )
+ )    (  )__)   )(   )   / )(_)(  )  (  \ (_) /
+(_/\/\_)(____) (__) (_)\_)(_____)(_)\_)(___\___)
+`.slice(1, -1);
+
+console.log(title);
+
 const system = new System<OtogeModel, OtogeSendable>(
   new OtogeModel(Path.resolve("./assets/songs"))
 );
